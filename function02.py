4c846d409c2cd2429919f644afefb5da7b283950
jyhh1992 <jyhh1992@gmail.com>
2021-11-23 12:15:39 +0900

    funtion2

---
+++ function02.py
@@ -0,0 +1,13 @@
+num1 = 10
+num2 = 20
+
+def fun(num01,num02):
+    plus = num01 + num02
+    minus = num01 -num02
+    multi = num01*num02
+    divide = num01/num02
+    return plus, minus, multi, divide
+
+a = fun(num1,num2)
+
+print(a)
