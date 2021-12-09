772575f748cde279b6931cd121d7fb7eefe26006
jyhh1992 <jyhh1992@gmail.com>
2021-12-09 17:55:57 +0900

    thread

--- opencv_flask_01.py
+++ opencv_flask_01.py
@@ -1,5 +1,6 @@
-from flask import Flask
+from flask import Flask, Response
 import cv2 as cv
+import threading
 
 app = Flask(__name__)
 
@@ -7,12 +8,39 @@ app = Flask(__name__)
 def helloworld():
     str = "Hello World!"
     return str
+
+global video_frame
+video_frame = None
+
+def encodeframe():
+    global video_frame
+    
+    while True:
+        ret, encoded_image = cv.imencode('.jpg', video_frame)
+        yield(b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + bytearray(encoded_image) + b'\r\n')
+    return
+
+@app.route("/streaming")    
+def streamframe():
+    return Response(encodeframe(), mimetype= "multipart/x-mixed-replace; boundary=frame")
+
 # /dev/video0
-if __name__ == '__main__':
+def captureframe():
+    # /dev/video0
+    global video_frame
     cap = cv.VideoCapture(0)
     while cap.isOpened():
-        ret , frame = cap.read()
-        cv.imshow("webcam",frame)
-        cv.waitKey(1)
+        ret, frame = cap.read()
+        # cv.imshow('webcam', frame)
+        video_frame = frame.copy()
+        cv.waitKey(30)
         pass
+    # return 
+
+if __name__ == '__main__':
+    cap_thread = threading.Thread(target=captureframe)
+    cap_thread.daemon = True
+    cap_thread.start()
+    # cap_thread.join()
     app.run(host='0.0.0.0', port='8000')
+    pass
