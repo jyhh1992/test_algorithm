56427aa0b8e1e5d062f00f8f03e7f635424a5c0d
jyhh1992 <jyhh1992@gmail.com>
2021-12-10 17:51:13 +0900

    roscom

---
+++ example02_pub.py
@@ -0,0 +1,20 @@
+#! /usr/bin/env python3
+
+import rospy
+from std_msgs.msg import String
+
+def fun():
+    return
+
+if __name__ == "__main__":
+    rospy.init_node('sample_pub')
+    pub = rospy.Publisher('hello2', String, queue_size=10)
+
+    rate = rospy.Rate(10)
+
+    while not rospy.is_shutdown():
+        str = 'hello_pubilsher2 : %s ' % rospy.get_time()
+        pub.publish(str)
+        rate.sleep()
+
+    pass
---
+++ example_sub.py
@@ -0,0 +1,16 @@
+#! /usr/bin/env python3
+
+import rospy
+from std_msgs.msg import String
+
+def fun_callback(msg):
+    rospy.loginfo("%s",msg.data)
+    return
+
+if __name__ == "__main__":
+    rospy.init_node('sample_sub')
+    rospy.Subscriber('hello',String,callback=fun_callback)
+    rospy.Subscriber('hello2',String,callback=fun_callback)
+    rospy.spin()
+    
+    pass
