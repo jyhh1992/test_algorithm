af2d0f5ffcddf62e49ea0caac67897956347a014
jyhh1992 <jyhh1992@gmail.com>
2021-12-31 14:11:30 +0900

    subcall

---
+++ call_copy.py
@@ -0,0 +1,6 @@
+from sub_call.file_call import response
+
+if __name__ == '__main__':
+    noname = response()
+    print(noname.call_result())
+    pass
---
+++ sub_call/file_call.py
@@ -0,0 +1,5 @@
+class response():
+    def __init__(self) -> None:
+        self.num = 5
+    def call_result(self):
+        return(self.num)
